6b7f32e074a174605e8c2e1b735b8af8fb7b3c77
realSahilYadav <sahil2006.gg@gmail.com>
2025-07-13 13:01:26 +0530

    Added database settings and scrips in frontend

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = ""
+load_dotenv()  # loads from .env
+
+username = os.getenv("DB_USERNAME")
+password = os.getenv("DB_PASSWORD")
+host = os.getenv("DB_HOST")
+dbname = os.getenv("DB_NAME")
+
+DATABASE_URL = f"mysql+pymysql://{username}:{password}@{host}/{dbname}"
+
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI(title="Drug Verification API")
+app = FastAPI(title="Aushadhi Rakshak")
 
 origins = ["https://aushadhi-rakshak.vercel.app/"
            #add frontend deployment link here
@@ -15,3 +15,14 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# --- Database ---
+from .database import Base, engine
+Base.metadata.create_all(bind=engine)
+
+# --- Routes ---
+from . import drugs
+app.include_router(drugs.router, prefix="/drugs", tags=["Drugs"])
+
+@app.get("/")
+def root():
+    return {"message": "Drug Verification System using DNA + FastAPI"}
